8164ff4107a653f75e005b59c5f4d89a523fde00
Thomas Curtis II <tcurtis@bgsu.edu>
2023-03-23 18:30:29 -0400

    Add files via upload

---
+++ programminglanguages/Assignment1/task4.rb
@@ -0,0 +1,8 @@
+#Author: Thomas Curtis
+#Program 4: Prints the string “The 9-th multiple of integer n is x” 
+
+for i in 5..13 do
+    
+    n = 9 * i
+    puts "The 9-th multiple of integer #{i} is #{n}"
+end
---
+++ programminglanguages/Assignment1/task7.c
@@ -0,0 +1,11 @@
+//Author: Thomas Curtis
+//Program 7: C program that outputs a statement
+
+#include <stdio.h>
+
+int main(){
+    
+    printf("Hello, class of CS 3060");
+
+    return 0;
+}
